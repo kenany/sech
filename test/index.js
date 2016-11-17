52beca4a7e9a9fb8ffe1194463123a9d5de4f983
Kenan Yildirim <kenan@kenany.me>
2016-11-17 12:43:53 -0500

    deps,test: use `almost-equal`
    Closes https://github.com/KenanY/sech/issues/3
    Closes https://github.com/KenanY/sech/issues/4

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var sech = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
+var almostEqual = require('almost-equal');
 
 test('exports a function', function(t) {
   t.plan(1);
@@ -13,5 +14,5 @@ test('sech', function(t) {
   t.equal(sech(1), 0.6480542736638855);
   t.equal(sech(2), 0.2658022288340797);
   t.equal(sech(3), 0.0993279274194332);
-  t.equal(sech(Math.PI), 0.08626673833405443);
+  t.ok(almostEqual(sech(Math.PI), 0.08626673833405443, 1e-16));
 });
