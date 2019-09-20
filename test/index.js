a7c13629c862578fdc863bed588923ff66dab29e
Kenan Yildirim <kenan@kenany.me>
2019-09-20 18:40:53 -0400

    deps: eslint@6.4.0, eslint-config@7.0.0
    Closes https://github.com/KenanY/sech/pull/24
    Closes https://github.com/KenanY/sech/pull/26

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
-var sech = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
 var almostEqual = require('almost-equal');
+
+var sech = require('../');
 
 test('exports a function', function(t) {
   t.plan(1);
